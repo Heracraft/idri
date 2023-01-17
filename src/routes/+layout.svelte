3a47d8980d8b059236cd157756f12194713561c6
Heracraft <heracraftgamer.ne@gmail.com>
2023-01-17 16:56:02 +0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,3 @@
+<script>
+    import "../style/tailwind.css"
+</script>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<article class="prose">
+    <h1>
+        hello Brocken ppl
+    </h1>
+    <p>
+        as we agreed we're dividing this into pages for each member
+    </p>
+    <p>packages i've installed </p>
+    <ul>
+        <li>tailwind-styleeee </li>
+        <li>netlify - hosting on netlify ,</li>
+        <li>@tailwindtypography-beatiful txt</li>
+    </ul>
+    <h6>NOTE:</h6>
+    <p>because we're working as a team ,before making any change run a git fetch then make a branch ukifanya changes na they work merge the code to the main brach</p>
+    <a href="https://www.youtube.com/watch?v=ecK3EnyGD8o&ab_channel=Fireship">More info</a>
+    <a href="https://www.youtube.com/watch?v=eB0nUzAI7M8">CI/CD</a>
+    <p>we'll work on ci/cd soon</p>
+</article>
