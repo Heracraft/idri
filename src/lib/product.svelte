70f5fecd6ebd5a7f80f478029fe7da9ef2ae04df
Heracraft <heracraftgamer.ne@gmail.comm>
2023-01-20 09:13:05 -0900

    fixed minor stuff

--- src/lib/product.svelte
+++ src/lib/product.svelte
@@ -2,9 +2,9 @@
     import Ratings from "./ratings.svelte";
 </script>
 
-<section class="prose lg:prose-xl flex flex-col items-center dark:text-white child:!my-2">
+<section class="prose lg:prose-xl flex flex-col items-center dark:child:text-white child:!my-2">
     <h1>Product card</h1>
-    <div class="flex flex-col max-w-sm p-5 child:!my-2 ">
+    <div class="flex flex-col max-w-sm p-5 card child:!my-2 ">
         <div class="flex justify-center">
             <img src="/apple-watch.png" alt="apple watch" class="max-w-[90%]"></div>
         <p class="font-semibold text-xl">
